2f61c973cb3924976caff03f4f0325cefc82b8dc
Hussain Afeef <ibnahnajjaar@gmail.com>
2025-09-06 23:13:25 +0500

    Remove unused employee-related functionality and components

--- routes/web.php
+++ routes/web.php
@@ -8,18 +8,4 @@ use App\Http\Controllers\Employee\EmployeeSocialiteController;
 Route::get('/admin/auth/{provider}/redirect', [AdminSocialiteController::class, 'redirect'])->name('admin.login.provider');
 Route::get('/admin/auth/{provider}/callback', [AdminSocialiteController::class, 'callback'])->name('admin.login.callback');
 
-Route::get('/employee/auth/{provider}/redirect', [EmployeeSocialiteController::class, 'redirect'])->name('employee.login.provider');
-Route::get('/employee/auth/{provider}/callback', [EmployeeSocialiteController::class, 'callback'])->name('employee.login.callback');
 
-
-Route::get('/test', function () {
-
-    return response()->json(
-        App\Models\KeplerTimeEntry::query()
-            ->orderBy('employee_id')
-            ->orderBy('date')
-            ->orderBy('entry_id')
-            ->orderBy('sort_order')
-        ->get()
-    );
-});
